015324cf0eee7c0b5ef16204e200934da3d2b45a
mesilucifer <ambika11176@gmail.com>
2022-08-29 18:06:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import ContactPage from './Pages/ContactPage';
+import Header from './Components/Header';
+import PortfolioPage from './Pages/PortfolioPage'
+import './App.css'
+
+function App() {
+    return (
+        <Router>
+        <Container className="p-0" fluid={true}>  
+        <Header/>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/portfolio" component={PortfolioPage}/>
+          <Route path="/contact" component={ContactPage}/>
+        
+        </Container>
+      </Router>
+    )
+}
+
+export default App
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+function HomePage() {
+    return (
+        <div className="homepage">
+          Home Page
+        </div>
+    )
+}
+
+export default HomePage
